dda0b1321e67dad63beced3c6e529961c9e79cf9
mark <mark77234@naver.com>
2026-01-29 20:01:25 +0900

    feat(colors): 색상 수정

--- constants/colors.ts
+++ constants/colors.ts
@@ -2,11 +2,11 @@ export type PrimaryShade = "100" | "200" | "300" | "400" | "500" | "600";
 
 export const primary: Record<PrimaryShade, string> = {
   "100": "#EDE9FF",
-  "200": "#DAD1FF",
-  "300": "#B7A6FF",
-  "400": "#8C76F6",
-  "500": "#684AE9",
-  "600": "#512FE2",
+  "200": "#DCD6FF",
+  "300": "#BFB2FF",
+  "400": "#9B87F5",
+  "500": "#6F52EB",
+  "600": "#4B24FF",
 };
 
 export const colors = {
